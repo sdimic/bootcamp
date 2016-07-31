f67ea1dce1a374050891c71ff8fd2194ea9f9ab4
Stefan Dimic <sdimic88@gmail.com>
2016-07-31 22:59:28 +0200

    replaced cursor with loader

--- app/src/main/java/com/stefandimic/contentresolverdemo/MainActivity.java
+++ app/src/main/java/com/stefandimic/contentresolverdemo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.stefandimic.contentresolverdemo;
 
 import android.app.ListActivity;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -8,26 +11,39 @@ import android.os.Bundle;
 import android.widget.ListAdapter;
 import android.widget.SimpleCursorAdapter;
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends ListActivity implements LoaderManager.LoaderCallbacks<Cursor>{
     private static final Uri CONTENT_URI =
             Uri.parse("content://com.stefandimic.provider.texts/texts");
 
-    private Cursor mCursor;
+    private SimpleCursorAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mCursor = getContentResolver().query(CONTENT_URI, null, null, null, null);
-        startManagingCursor(mCursor);
-
-        ListAdapter adapter = new SimpleCursorAdapter(
+        mAdapter = new SimpleCursorAdapter(
                 this,
                 android.R.layout.two_line_list_item,
-                mCursor,
+                null,
                 new String[] {"_id", "text"},
                 new int[] {android.R.id.text1, android.R.id.text2});
 
-        setListAdapter(adapter);
+        setListAdapter(mAdapter);
+        getLoaderManager().initLoader(0, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return new CursorLoader(this, CONTENT_URI, null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        mAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(Loader loader) {
+        mAdapter.swapCursor(null);
     }
 }
